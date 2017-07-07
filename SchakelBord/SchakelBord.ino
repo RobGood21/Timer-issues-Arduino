6245419804a6977d7153de565b22a04d030b79bf
Rob Antonisse <robantonisse@gmail.com>
2017-07-07 22:21:38 +0200

    SchakelBord created

---
+++ SchakelBord/SchakelBord.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		SchakelBord.ino
+ Created:	7/7/2017 10:20:39 PM
+ Author:	Rob Antonisse
+ Info:		www.wisselmotor.nl
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
